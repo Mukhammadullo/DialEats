a154627c5e1b84346c9919e26bdf0da4187c781d
Muhammadullo <mnastulloev@gmail.com>
2023-12-11 19:17:12 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import img1 from '../src/assets/img1.png'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Empty Elements</h1>
+
+      <div className='header w-[100%] h-[80vh] border-[2px]  '>
+       <h1>mekrlmrkkergmdkg,r gkrg rtkg rkmgr kr</h1>
+      </div>
+
     </>
   )
 }
