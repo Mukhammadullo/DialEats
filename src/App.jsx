d26f2a5ac51202d8c5c0fb8003e3896880a7ef73
Muhammadullo <mnastulloev@gmail.com>
2023-12-11 19:37:55 +0500

    fix1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import img1 from '../src/assets/img1.png'
-
+import logotip from '../src/assets/logotip.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +16,10 @@ function App() {
       <div className='header w-[100%] h-[80vh] border-solid  border-[2px] border-red-600  '>
 
         {/* card1_header */}
-        <div></div>
+        <div className='w-[100%] h-[10vh] border-solid border-[2px] border-blue-900 flex justify-between  items-center' >
+          <img src={logotip} className='w-[200px] ' />
+          <p className='text-white'>Privacy Policy</p>
+        </div>
 
       </div>
 
