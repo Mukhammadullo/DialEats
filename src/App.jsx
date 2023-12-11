28231599f9c43d26246feb6d1b5b0397e41788ec
Muhammadullo <mnastulloev@gmail.com>
2023-12-11 19:55:12 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 
 import img1 from '../src/assets/img1.png'
 import logotip from '../src/assets/logotip.png'
+import apple from '../src/assets/apple 1.png'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,7 +27,13 @@ function App() {
         {/* card1_cards */}
         <div className='flex'>
           {/* card1_left */}
-          <div className='w-[50%] h-[70vh] border-solid border-[2px] border-red-700'></div>
+          <div className='w-[50%] h-[70vh] border-solid border-[2px] border-red-700'>
+            <h1 className='font-mono text-[45px] text-white text-bold'>Why stay hungry when you can order from DialEats</h1>
+            <p className='text-white'>Download the bella onoje’s food app now on</p>
+            <div>
+             <div></div>
+            </div>
+          </div>
 
           {/* card1_right */}
           <div className='w-[50%] h-[70vh] border-solid border-[2px] border-red-700'></div>
