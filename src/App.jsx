46850fa6da6d5ce993a9823acf8d93e91f60a2cd
Muhammadullo <mnastulloev@gmail.com>
2023-12-12 08:17:17 +0500

    card2_style

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ import apple from '../src/assets/apple 1.png'
 import playmarket from '../src/assets/google-play 1.png'
 import phoneLeft from '../src/assets/Rectangle4 1.png'
 import phoneRight from '../src/assets/Rectangle6 1.png'
+import card2_inner1 from '../src/assets/i (1).png'
+import card2_inner2 from '../src/assets/i (2).png'
+import card2_inner3 from '../src/assets/i (3).png'
+import card2_inner4 from '../src/assets/i (4).png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,10 +21,10 @@ function App() {
     <>
 
       {/* card1 */}
-      <div className='header w-[100%] h-[80vh] border-solid  border-[2px] border-red-600  '>
+      <div className='header w-[100%] h-[80vh]'>
 
         {/* card1_header */}
-        <div className='w-[100%] h-[10vh] border-solid border-[2px] border-blue-900 flex justify-between  items-center' >
+        <div className='w-[100%] h-[10vh]  flex justify-between  items-center' >
           <img src={logotip} className='w-[200px] ml-[10px]' />
           <p className='text-white mr-[10px]'>Privacy Policy</p>
         </div>
@@ -29,7 +33,7 @@ function App() {
         {/* card1_cards */}
         <div className='flex'>
           {/* card1_left */}
-          <div className='w-[50%] h-[70vh] border-solid border-[2px] border-red-700 flex justify-center  flex-col'>
+          <div className='w-[50%] h-[70vh]  flex justify-center  flex-col'>
             <h1 className='text-[45px] text-white font-bold ml-[15px]'>Why stay hungry when you can order from DialEats</h1>
             <p className='text-[#F6F6F6] m-[15px]'>Download the bella onoje’s food app now on</p>
 
@@ -54,9 +58,8 @@ function App() {
           </div>
 
           {/* card1_right */}
-          <div className='w-[50%] h-[70vh] border-solid border-[2px] border-red-700'>
-
-            <div className='flex justify-center w-[100%] border-[2px] border-solid border-red-600 h-[85vh] '>
+          <div className='w-[50%] h-[70vh]'>
+            <div className='flex justify-center w-[100%]  h-[85vh] '>
               <img src={phoneLeft} className='w-[60%] h-[100vh]' />
               <img src={phoneRight} className='w-[40%] h-[80vh] ' />
             </div>
@@ -64,6 +67,19 @@ function App() {
           </div>
         </div>
       </div>
+
+
+      {/* card2_________ */}
+      <div className='w-[100%] h-[40vh] mt-[110px] flex justify-around items-center'>
+        <div className='card2_inner1 w-[22%] h-[35vh] rounded-[12px] flex flex-col justify-end items-center'><p className='text-white'>Harere</p></div>
+        <div className='card2_inner2 w-[22%] h-[35vh] rounded-[12px] flex flex-col justify-end items-center'><p className='text-white'>Buluwayo</p></div>
+        <div className='card2_inner3 w-[22%] h-[35vh] rounded-[12px] flex flex-col justify-end items-center'><p className='text-white'>Mature</p></div>
+        <div className='card2_inner4 w-[22%] h-[35vh] rounded-[12px] flex flex-col justify-end items-center'><p className='text-white'>Chinhoyi</p></div>
+      </div>
+
+
+      {/* card3__________ */}
+
 
     </>
   )
